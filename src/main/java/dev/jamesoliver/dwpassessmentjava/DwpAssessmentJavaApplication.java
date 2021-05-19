6ceaa6a5b1097720ef05800cfb21bd92ba538d81
James Oliver <joliver.robert@me.com>
2021-05-19 17:25:56 +0100

    chore(core): adds initial intellij project generation files

---
+++ src/main/java/dev/jamesoliver/dwpassessmentjava/DwpAssessmentJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.jamesoliver.dwpassessmentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwpAssessmentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwpAssessmentJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jamesoliver/dwpassessmentjava/DwpAssessmentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jamesoliver.dwpassessmentjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwpAssessmentJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
