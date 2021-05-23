c267f6e0ee7677aa07e2a4222b5bb3f0d6296357
James Oliver <joliver.robert@me.com>
2021-05-23 09:40:41 +0100

    refactor(core): renames application entrypoint

--- src/main/java/dev/jamesoliver/dwpassessmentjava/Application.java
+++ src/main/java/dev/jamesoliver/dwpassessmentjava/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DwpAssessmentJavaApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DwpAssessmentJavaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
